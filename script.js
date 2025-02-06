d0755fe5926df6e0f51a4bc6284b9e0d9e193246
ChideraEmmanuelCE <142711382+ChideraEmmanuelCE@users.noreply.github.com>
2025-02-06 22:57:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Fuck you from the bottom of my broken heart');
+});
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('I love you from the bottom of my bottom heart');
+});
