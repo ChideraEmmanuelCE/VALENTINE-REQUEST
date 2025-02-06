233361620ce69b66b430a14ea7497bc346b1a06e
ChideraEmmanuelCE <142711382+ChideraEmmanuelCE@users.noreply.github.com>
2025-02-06 23:10:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('myButton').addEventListener('click', function() {
-    alert('You"'"re ugly by the way');
+    alert('You are ugly SO?');
 });
 document.getElementById('yesButton').addEventListener('click', function() {
     alert('I love you from the bottom of my heart');
