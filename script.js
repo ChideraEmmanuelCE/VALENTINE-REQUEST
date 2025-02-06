5dde98f946c9b0ce6ae60312a44cb6fa04a34973
ChideraEmmanuelCE <142711382+ChideraEmmanuelCE@users.noreply.github.com>
2025-02-06 23:08:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('myButton').addEventListener('click', function() {
-    alert('Fuck you from the bottom of my broken heart');
+    alert('You"'"re ugly by the way');
 });
 document.getElementById('yesButton').addEventListener('click', function() {
-    alert('I love you from the bottom of my bottom heart');
+    alert('I love you from the bottom of my heart');
 });
